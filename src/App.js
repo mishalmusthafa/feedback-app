c38ed2f8224fbe90d8afec2b78f00ec191d6b98c
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-03-28 17:38:44 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
+import Header from "./components/Header";
+
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my blog post';
-  const comments = [
-    { id: 1, text: 'This is comment 1' },
-    { id: 2, text: 'This is comment 2' },
-    { id: 3, text: 'This is comment 3' },
-  ];
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      <div className="comments">
-        <h3>comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={comment.id}>{comment.text}</li>
-            //Above is same as '=> {return <li>{comment.text}</li>}'
-          ))}
-
-        </ul>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
       </div>
-    </div>
+    </>
   );
 }
 
-export default App;
+export default App;;
