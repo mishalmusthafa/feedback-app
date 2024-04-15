d69ead50b4f24a3b64b1b7aba4f0e9fe8c9a8574
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-04-15 17:52:41 +0530

    Added isLoading state for the loading animation as a boolean in feedbackContext

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,28 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 8,
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 7,
-    },
-  ]);
-
+  const [feedback, setFeedback] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch data from database
+  const fetchFeedback = async () => {
+    const response = await fetch('http://localhost:5000/feedback?sort=id&_order=desc');
+
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   // Update feedback
   const updateFeedback = (id, updItem) => {
@@ -62,6 +59,7 @@ export const FeedbackProvider = ({ children }) => {
     {
       feedback,
       feedbackEdit,
+      isLoading,
       deleteFeedback,
       addFeedback,
       editFeedback,
